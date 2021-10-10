6e8455b730b21781a6d0845b620d0cc2346cc8c3
jiefeng <lowjiefeng1998@gmail.com>
2021-10-11 01:07:42 +0800

    Add handlers for room joining/leaving

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -5,6 +5,7 @@ import {
   ConnectedSocket,
   WebSocketServer,
   MessageBody,
+  WsException,
 } from '@nestjs/websockets';
 import { UsersService } from './users/users.service';
 import { UseGuards } from '@nestjs/common';
@@ -16,6 +17,7 @@ import { UserWsId } from './auth/user.decorator';
 import { VetoedParticipantsDto } from './challenges/dto/vetoed-participants.dto';
 import { WsLogger } from './middleware/ws-logger.middleware';
 import { AppEmitter } from './app.emitter';
+import { ChallengeData } from './challenges/entities/challenge.entity';
 
 export const EVENTS = {
   connection: 'connection',
@@ -24,6 +26,8 @@ export const EVENTS = {
   challengeReject: 'challengeReject',
   challengeAccept: 'challengeAccept',
   challengeReleaseResults: 'challengeReleaseResults',
+  roomJoin: 'roomJoin',
+  roomLeave: 'roomLeave',
 };
 
 @WebSocketGateway({ transports: ['websocket', 'polling'] })
@@ -40,27 +44,118 @@ export class AppGateway implements OnGatewayConnection {
   server: Server;
 
   async handleConnection(socket: Socket) {
-    this.wsLogger.log(socket, EVENTS.connection, 'EMIT');
     socket.emit(EVENTS.connection, { clientId: socket.id }); // success message
+    this.wsLogger.log(socket, EVENTS.connection, 'EMIT');
   }
 
   /**
+   * Handles the globalLeaderboard event.
+   *
    * Expecting:
    *
    * ```
    * { } // ie nothing
    * ```
+   *
+   * Returns:
+   *  ACK:
+   *    <NONE>
+   *  EMITS:
+   *    ON globalLeaderboard event :: TYPE UserList[]
    */
   @UseGuards(JwtWsAuthGuard)
   @SubscribeMessage(EVENTS.globalLeaderboard)
   async getGlobalLeaderboard(@ConnectedSocket() socket: Socket) {
     this.wsLogger.log(socket, EVENTS.globalLeaderboard, 'RECEIVE');
     const results: UserList[] = await this.usersService.getGlobalLeaderboard();
-    this.wsLogger.log(socket, EVENTS.globalLeaderboard, 'EMIT', results);
     socket.emit(EVENTS.globalLeaderboard, results);
+    this.wsLogger.log(socket, EVENTS.globalLeaderboard, 'EMIT');
   }
 
   /**
+   * Handles the roomJoin event.
+   *
+   * Expecting:
+   *
+   * ```
+   * {
+   *  challengeId: string,
+   * }
+   * ```
+   *
+   * Returns:
+   *  ACK:
+   *    TYPE ChallengeData
+   *  EMITS:
+   *    TODO
+   */
+  @UseGuards(JwtWsAuthGuard)
+  @SubscribeMessage(EVENTS.roomJoin)
+  async joinRoom(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody('challengeId') challengeId: string,
+  ) {
+    this.wsLogger.log(socket, EVENTS.roomJoin, 'RECEIVE');
+    if (challengeId) {
+      try {
+        const challengeData: ChallengeData =
+          await this.challengesService.findOne(challengeId);
+
+        socket.join(challengeId);
+
+        // TODO: broadcast
+
+        this.wsLogger.log(socket, EVENTS.roomJoin, 'EMIT');
+        return challengeData;
+      } catch (e) {
+        console.log(e);
+        throw new WsException('ChallengeId not found');
+      }
+    }
+  }
+
+  /**
+   * Handles the roomLeave event.
+   *
+   * Expecting:
+   *
+   * ```
+   * {
+   *  challengeId: string,
+   * }
+   * ```
+   *
+   * Returns:
+   *  ACK:
+   *    TYPE { roomId: string }
+   *  EMITS:
+   *    TODO
+   */
+  @UseGuards(JwtWsAuthGuard)
+  @SubscribeMessage(EVENTS.roomLeave)
+  async leaveRoom(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody('challengeId') challengeId: string,
+  ) {
+    this.wsLogger.log(socket, EVENTS.roomLeave, 'RECEIVE');
+    if (challengeId) {
+      try {
+        socket.leave(challengeId);
+
+        // TODO: broadcast
+
+        this.wsLogger.log(socket, EVENTS.roomLeave, 'EMIT');
+        return { roomId: challengeId };
+      } catch (e) {
+        console.log(e);
+        // do nothing
+      }
+    }
+  }
+
+  /**
+   * Handles the challengeAccept event.
+   *
    * Expecting:
    *
    * ```
@@ -77,10 +172,35 @@ export class AppGateway implements OnGatewayConnection {
     @MessageBody('challengeId') challengeId: string,
   ) {
     this.wsLogger.log(socket, EVENTS.challengeAccept, 'RECEIVE');
-    await this.challengesService.acceptChallenge(userId, challengeId);
+
+    try {
+      await this.challengesService.acceptChallenge(userId, challengeId);
+    } catch (error) {
+      console.log(error);
+      throw new WsException('Acceptance of challenge failed');
+    }
+
+    try {
+      // TODO: refactor as this is same code as join room
+      const challengeData: ChallengeData = await this.challengesService.findOne(
+        challengeId,
+      );
+
+      socket.join(challengeId);
+
+      // TODO: broadcast
+
+      this.wsLogger.log(socket, EVENTS.roomJoin, 'EMIT');
+      return challengeData;
+    } catch (error) {
+      console.log(error);
+      throw new WsException('Failed to join room');
+    }
   }
 
   /**
+   * Handles the challengeReject event.
+   *
    * Expecting:
    *
    * ```
@@ -97,7 +217,17 @@ export class AppGateway implements OnGatewayConnection {
     @MessageBody('challengeId') challengeId: string,
   ) {
     this.wsLogger.log(socket, EVENTS.challengeReject, 'RECEIVE');
-    await this.challengesService.rejectChallenge(userId, challengeId);
+
+    try {
+      await this.challengesService.rejectChallenge(userId, challengeId);
+      socket.leave(challengeId);
+
+      this.wsLogger.log(socket, EVENTS.challengeReject, 'EMIT');
+      return { roomId: challengeId };
+    } catch (error) {
+      console.log(error);
+      throw new WsException('Rejection of challenge failed');
+    }
   }
 
   /**
@@ -117,7 +247,20 @@ export class AppGateway implements OnGatewayConnection {
     @MessageBody('challengeId') challengeId: string,
   ) {
     this.wsLogger.log(socket, EVENTS.challengeComplete, 'RECEIVE');
-    await this.challengesService.completeChallenge(userId, challengeId);
+
+    try {
+      await this.challengesService.completeChallenge(userId, challengeId);
+    } catch (error) {
+      console.log(error);
+      throw new WsException('Completion of challenge failed');
+    }
+
+    try {
+      // TODO: broadcast
+    } catch (error) {
+      console.log(error);
+      throw new WsException('Failed to notify room');
+    }
   }
 
   /**
@@ -147,6 +290,7 @@ export class AppGateway implements OnGatewayConnection {
       data,
       'ws',
     );
+
     await this.appEmitter.releaseResultsNotify(
       this.server,
       challengeId,
