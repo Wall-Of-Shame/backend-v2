97b83d37ca7e28e3ad2ccfb7958c10982f6220d5
jiefeng <lowjiefeng1998@gmail.com>
2021-10-27 02:57:07 +0800

    Add logic for sending emails

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,6 +1,7 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { v2 as cloudinary } from 'cloudinary';
 import { Injectable } from '@nestjs/common';
+import { Challenge, User } from '.prisma/client';
 
 @Injectable()
 export class MailService {
@@ -35,5 +36,55 @@ export class MailService {
         imageUrl,
       },
     });
+  }
+
+  async sendInvites(
+    challenge: Challenge,
+    owner: User,
+    recipients: User[],
+  ): Promise<void> {
+    const { title, description, startAt, endAt } = challenge;
+    const { username, name } = owner;
+
+    return;
+
+    /*
+    await Promise.all(
+      recipients.map(async (receipient) => {
+        await this.mailerService.sendMail({
+          to: receipient.email,
+          subject: `Wall Of Shame - Invitation to join ${challenge.title}`,
+          template: './invitation',
+          context: {
+            title,
+            description,
+            startAt,
+            endAt,
+            ownerUsername: username,
+            ownerName: name,
+            recipientUsername: receipient.username,
+            recipientName: receipient.name,
+          },
+        });
+      }),
+    );
+    */
+  }
+
+  async sendFriendRequests(sender: User, receipient: User): Promise<void> {
+    return;
+    /*
+    await this.mailerService.sendMail({
+      to: receipient.email,
+      subject: `Wall Of Shame - Friend request from ${sender.name}`,
+      template: './friendRequest',
+      context: {
+        senderUsername: sender.username,
+        senderName: sender.name,
+        recipientUsername: receipient.username,
+        recipientName: receipient.name,
+      },
+    });
+    */
   }
 }
