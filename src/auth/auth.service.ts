b168981f2aedb87fa48ea2bdb8297b1a1f199f78
jiefeng <lowjiefeng1998@gmail.com>
2021-10-29 23:24:21 +0800

    Add random usernames

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { AvatarAnimal, AvatarColor, User } from '@prisma/client';
 import admin from 'firebase-admin';
+import { datatype, random } from 'faker';
 import { UserData } from '../users/entities/user.entity';
 import { UsersService } from '../users/users.service';
 
 export interface JwtPayload {
   sub: string; // userId
 }
+
+type RandomUserInfo = Pick<
+  User,
+  'name' | 'username' | 'avatar_animal' | 'avatar_color' | 'avatar_bg'
+>;
 
 @Injectable()
 export class AuthService {
@@ -21,10 +28,13 @@ export class AuthService {
     let user: UserData;
     user = await this.usersService.findOne({ email });
     if (!user) {
+      const randomUserInfo = await this.randomUserInfo();
       user = await this.usersService.create({
+        ...randomUserInfo,
         email,
         messagingToken,
       });
+
       user = await this.usersService.findOne({ email });
     } else {
       await this.usersService.updateToken(email, {
@@ -50,5 +60,37 @@ export class AuthService {
     } catch (error) {
       return null;
     }
+  }
+
+  private async randomUserInfo(): Promise<RandomUserInfo> {
+    const avatar_animal = random.arrayElement([
+      AvatarAnimal.CAT,
+      AvatarAnimal.DOG,
+      AvatarAnimal.RABBIT,
+    ]);
+    const avatar_color = random.arrayElement([
+      AvatarColor.PRIMARY,
+      AvatarColor.SECONDARY,
+      AvatarColor.TERTIARY,
+    ]);
+    const avatar_bg = random.arrayElement(['#cbe8e0', '#c9b2e1', '#c2d5eb']);
+
+    let name: string;
+    let username: string;
+    let randomNumber: number;
+
+    do {
+      randomNumber = datatype.number({ min: 100000, max: 999999 });
+      name = `User#${randomNumber}`;
+      username = name;
+    } while ((await this.usersService.findOne({ username })) !== null);
+
+    return {
+      name,
+      username,
+      avatar_animal,
+      avatar_bg,
+      avatar_color,
+    };
   }
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { AvatarAnimal, AvatarColor } from '.prisma/client';
+
 export class CreateUserDto {
+  username: string;
+  name: string;
+  avatar_animal: AvatarAnimal;
+  avatar_color: AvatarColor;
+  avatar_bg: string;
   email: string;
   messagingToken?: string | undefined;
 }
